524cdc765a2f68534733133f648c0d0bdd06eab2
ajpkim swift3 <alexkim345@gmail.com>
2021-03-12 11:07:24 -0500

    fin floating toc via js btn handling, css, html default page layout

---
+++ assets/js/floating-toc.js
@@ -0,0 +1,25 @@
+// 2020-12-04 
+
+
+// Copy text-Table-of-Contents into #floating-toc
+
+// On cloneNode func: https://developer.mozilla.org/en-US/docs/Web/API/Node/cloneNode
+const toc = document.querySelector("#text-Table-of-Contents");
+console.log(toc);
+let floatingToc = toc.cloneNode(true);
+floatingToc.id = "floating-toc";
+document.body.appendChild(floatingToc);
+
+
+// Toggle floating TOC based on button
+
+tocButton  = document.querySelector('#toc-btn');
+tocButton.addEventListener('click', (e) => {
+    if (tocButton.textContent === '+') {
+        tocButton.textContent = '-'
+        floatingToc.style.display = 'block'
+    } else {
+        tocButton.textContent = '+'
+        floatingToc.style.display = 'none';
+    }
+})
