70585a177740e1ff296c94cbdf2308ad404b6fc1
ajpkim swift3 <alexkim345@gmail.com>
2021-05-19 14:32:54 -0400

    changed containers to ids

--- assets/js/floating-toc.js
+++ assets/js/floating-toc.js
@@ -4,8 +4,7 @@
 // Copy text-Table-of-Contents into #floating-toc
 
 // On cloneNode func: https://developer.mozilla.org/en-US/docs/Web/API/Node/cloneNode
-const toc = document.querySelector("#text-Table-of-Contents");
-console.log(toc);
+const toc = document.querySelector('#text-Table-of-Contents');
 let floatingToc = toc.cloneNode(true);
 floatingToc.id = "floating-toc";
 document.body.appendChild(floatingToc);
@@ -14,6 +13,8 @@ document.body.appendChild(floatingToc);
 // Toggle floating TOC based on button
 
 tocButton  = document.querySelector('#toc-btn');
+// const toc = document.querySelector('#text-Table-of-Contents');
+
 tocButton.addEventListener('click', (e) => {
     if (tocButton.textContent === '+') {
         tocButton.textContent = '-'
